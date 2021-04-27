debbaadd56bf29335392f9b415e629fa5601503d
AIMEE GARRIOCK <AIMEEGARRIOCK@gmail.com>
2021-04-26 22:05:56 -0700

    added vertical search

--- wordsearch.js
+++ wordsearch.js
@@ -3,6 +3,13 @@ const wordSearch = (letters, word) => {
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
-}
+
+    const verticalJoin = letters[0].map((col, i) => letters.map(row => row[i])).map(ls => ls.join(''));
+    for (const l of verticalJoin) {
+        if (l.includes(word)) return true;
+    }
+    return false;
+  };
+
 
 module.exports = wordSearch
